7fce46a1edf47641e10b3f7917ff748221f073ad
chamnabmey <chamnabmey.info@gmail.com>
2025-11-20 22:49:28 +0700

    Use correct Prisma client path in seed script

--- scripts/seed-database.ts
+++ scripts/seed-database.ts
@@ -4,7 +4,7 @@
  */
 
 import "dotenv/config";
-import { PrismaClient } from "@prisma/client";
+import { PrismaClient } from "../src/generated/prisma/client.js";
 
 const prisma = new PrismaClient();
 
